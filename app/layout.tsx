006eaeaf06417d60934474e821784193298454a5
James <shockleyje@protonmail.com>
2024-09-05 15:14:14 -0400

    test automatic deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  description: "The fastest way to build apps with Next.js, Supabase, and James :^)",
 };
 
 export default function RootLayout({
